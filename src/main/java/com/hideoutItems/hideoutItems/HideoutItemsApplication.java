31af814680a80a18c953b6dd52b89dc5bf817d16
mmiiranda <mauricio201190b@gmail.com>
2025-03-03 12:29:50 -0300

    initial commit: init Spring Boot setup

---
+++ src/main/java/com/hideoutItems/hideoutItems/HideoutItemsApplication.java
@@ -0,0 +1,14 @@
+package com.hideoutItems.hideoutItems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideoutItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HideoutItemsApplication.class, args);
+	}
+
+			
+}
---
+++ src/test/java/com/hideoutItems/hideoutItems/HideoutItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hideoutItems.hideoutItems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HideoutItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
